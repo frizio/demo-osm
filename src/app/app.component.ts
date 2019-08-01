d196cb3cc59857f3529839d8c94d1f367c6168b8
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-08-01 11:55:11 +0200

    Create and Configure a Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng, circle, polygon } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-osm';
+
+  options = {
+    layers: [
+      tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          maxZoom: 18,
+          attribution: '&copy; OpenStreetMap contributors'
+        }
+      )
+    ],
+    zoom: 5,
+    center: latLng(38.1234, 13.1234)
+  };
+
 }
